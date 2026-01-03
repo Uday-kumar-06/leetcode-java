5c385b4d15f409eb75a7be015e119ec54b03e5d4
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-01-04 01:12:30 +0530

    Wrote the Code for leetcode problem number 3

--- strings/LongestSubstring_03.java
+++ strings/LongestSubstring_03.java
@@ -1,3 +1,39 @@
+import java.util.HashSet;
+
 public class LongestSubstring_03 {
-    
+
+    public static int lengthOfLongestSubstring(String s) {
+        int left = 0, right = 0, longest = 0;
+        HashSet<Character> set = new HashSet<>();
+
+        while (right < s.length()) {
+            if (!set.contains(s.charAt(right))) {
+                set.add(s.charAt(right));
+                longest = Math.max(longest, right - left + 1);
+                right++;
+            } else {
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        String[] testCases = {
+                "abcabcbb",
+                "bbbbb",
+                "pwwkew",
+                "",
+                " ",
+                "abba"
+        };
+
+        for (String s : testCases) {
+            System.out.println(
+                    "Input: \"" + s + "\" -> Longest Substring Length = "
+                            + lengthOfLongestSubstring(s)
+            );
+        }
+    }
 }
