e8ed72e3b92b4f5f59cc57c15920453c1537abd3
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-01-16 21:39:36 +0530

    Just wrote logic for 455 question inleetcode

---
+++ greedy/AssignCookies_455.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0; // child pointer
+        int j = 0; // cookie pointer
+        int count = 0;
+
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                count++;   // child satisfied
+                i++;
+                j++;
+            } else {
+                j++; // cookie too small, try bigger cookie
+            }
+        }
+        return count;
+    }
+}
