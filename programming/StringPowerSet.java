d09a9704432dbb6f420dec6912cca951a0305add
Uday-kumar-06 <130353691+Uday-kumar-06@users.noreply.github.com>
2026-03-04 11:04:56 +0530

    Update StringPowerSet.java

--- programming/StringPowerSet.java
+++ programming/StringPowerSet.java
@@ -1,1 +1,19 @@
-
+import java.util.*;
+public class Main
+{
+    public static void powerSet(int i,String str,String str1){
+        if(i == str1.length()){
+            System.out.println(str);
+            return;
+        }
+        powerSet(i+1,str+str1.charAt(i),str1);
+        
+        powerSet(i+1,str,str1);
+    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str1 = sc.next();
+		
+		powerSet(0,"",str1);
+	}
+}
